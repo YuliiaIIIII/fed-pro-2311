5615f83986eb47bd9e7d75874dbeeae2334d1612
YuliiaIIIII <yulyamashka@gmail.com>
2022-12-24 12:46:30 +0100

    HW 4. Objects. 3,5 done

--- HW 04/objects.js
+++ HW 04/objects.js
@@ -1,15 +1,30 @@
 // 1)Створити обєкт userWallet  в якому існують ключі (amountUsa, amountEuro, amountUa) які містять числові значення ( збереження юзера) Створити массив обєктів bank в якому є обєкети з ключами buy, sell  з числовими значеннями ( продаж і покупка валюти) а також ключ name в який передаємо назву валюти (usa, euro, ua) Створити функцію яка отримує данні з обєкту юзер опрацьовує ці данні і повертає скільки долларів чи євро може купити користувач, врахувати можливість неможливості покупки. Створити функцію яка прораховує скільки гривень отримує користувач продавши свої збереження
+
 // let userWallet{
-//   amountUsa, 
-//   amountEuro, 
-//   amountUa
+//   amountUsa: , 
+//   amountEuro: , 
+//   amountUa: 
 // }
 // bank = [];
+
 
 // 2)Створити функцію move яка повертає текст на скільки кроків і куди (отримане значення) змістився користувач . Створити функцію moveUser яка отримує напрямок переміщення, кількість кроків і функцію move як колбек. moveUser ('north', move, 10) повина повернути ( Юзер перемістився на північ на 10 кроків)
 
 
+
 // 3) Створіть массив в якому видаляється кожний другий елемент ["Keep", "Remove", "Keep", "Remove", "Keep", ...] в результаті повинен бути ось такий новий массив ["Keep", "Keep", "Keep", ...], Врахувати що массив може бути пустий, повернути помилку в разі пустого масиву
+let elements = ["Keep", "Remove", "Keep", "Remove", "Keep", "Remove", "Keep", "Remove", "Keep", "Remove", "Keep"];
+// let elements = [];
+function removeElements(){
+  if(!elements.length){
+    alert("Empty array");
+  }
+  for(i=1; i<=elements.length; i++){
+    elements.splice(i, 1);
+  }
+  console.log(elements);
+}
+removeElements();
 
 
 // 4) Створити функцію яка обробляє массив обєктів і вираховує площу фігури в обєкті,
@@ -34,6 +49,9 @@ let multiArr = [];
 for(let n=0; n<NumbersArr.length; n++){
   if(NumbersArr[n]%2 == 0){
     let multiple = NumbersArr[n] * 4;
-    console.log(multiple)
+    // console.log(multiple);
+    multiArr.push(multiple);
+    // console.log(multiArr);
   }
 }
+console.log(multiArr)
