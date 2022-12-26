3e2317d2aff43584c222bad7519ff81f6d72bda5
YuliiaIIIII <yulyamashka@gmail.com>
2022-12-26 14:24:18 +0100

    HW 4. functions, objects, callback-funktions

--- HW 04/objects.js
+++ HW 04/objects.js
@@ -1,17 +1,68 @@
-// 1)Створити обєкт userWallet  в якому існують ключі (amountUsa, amountEuro, amountUa) які містять числові значення ( збереження юзера) Створити массив обєктів bank в якому є обєкети з ключами buy, sell  з числовими значеннями ( продаж і покупка валюти) а також ключ name в який передаємо назву валюти (usa, euro, ua) Створити функцію яка отримує данні з обєкту юзер опрацьовує ці данні і повертає скільки долларів чи євро може купити користувач, врахувати можливість неможливості покупки. Створити функцію яка прораховує скільки гривень отримує користувач продавши свої збереження
+// 1)Створити обєкт userWallet  в якому існують ключі (amountUsa, amountEuro, amountUa) які містять числові значення ( збереження юзера) Створити массив обєктів bank в якому є обєкти з ключами buy, sell  з числовими значеннями ( продаж і покупка валюти) а також ключ name в який передаємо назву валюти (usa, euro, ua) Створити функцію яка отримує данні з обєкту юзер опрацьовує ці данні і повертає скільки долларів чи євро може купити користувач, врахувати можливість неможливості покупки. Створити функцію яка прораховує скільки гривень отримує користувач продавши свої збереження
+let userWallet = {
+  amountUsd: 0, 
+  amountEuro: 0, 
+  amountUah: 0 
+}
 
-// let userWallet{
-//   amountUsa: , 
-//   amountEuro: , 
-//   amountUa: 
-// }
-// bank = [];
+bank = [
+    {
+        name: "usa",
+        buy: 40.5,
+        sell: 40.0
+    },
+    {
+        name: "euro",
+        buy: 42.5,
+        sell: 41.75
+    },
+    {
+        name: "uah",
+        buy: 0.027,
+        sell: 0.025
+    }
+];
+
+userWallet.amountUsd = parseFloat(prompt("Вкажіть скільки USD покласти до гаманця:"));
+userWallet.amountEuro = parseFloat(prompt("Вкажіть скільки EURO покласти до гаманця:"));
+userWallet.amountUah = parseFloat(prompt("Вкажіть скільки UAH покласти до гаманця:"));
+
+function converter(){
+    if(userWallet.amountUah<=0){
+        alert("Your balance is too low!");
+    }
+    return `За ${userWallet.amountUah} UAH ви можете купити ${(userWallet.amountUah / bank[0].buy).toFixed(2)} USD.\nЗа ${userWallet.amountUah} UAH ви можете купити ${(userWallet.amountUah / bank[1].buy).toFixed(2)} EU.`; 
+}
+converter(userWallet);
+console.log(converter());
+
+function sellAllAmount(){
+    let getUAHfromUsd = parseFloat((userWallet.amountUsd * bank[0].sell).toFixed(2));
+    let getUAHfromEuro = parseFloat((userWallet.amountEuro * bank[1].sell).toFixed(2));
+    let sellAll = parseFloat((getUAHfromUsd + getUAHfromEuro).toFixed(2));
+    return `Продаж збережень в долларах принесе Вам ${getUAHfromUsd} UAH.\nПродаж збережень в євро принесе Вам ${getUAHfromEuro} UAH.\nПродаж всіх збережень принесе Вам ${sellAll} UAH.` 
+
+}
+sellAllAmount();
+console.log(sellAllAmount());
+
 
 
 // 2)Створити функцію move яка повертає текст на скільки кроків і куди (отримане значення) змістився користувач . Створити функцію moveUser яка отримує напрямок переміщення, кількість кроків і функцію move як колбек. moveUser ('north', move, 10) повина повернути ( Юзер перемістився на північ на 10 кроків)
+let direction = prompt("Вкажіть напрямок переміщення (west, east, north, south):");
+let steps = parseInt(prompt("Вкажіть кількість кроків:"));
 
+function move(){
+    return `Юзер перемістився на ${direction} на ${steps} кроків`
+}
+move();
 
+function moveUser(fn){ 
+    console.log(fn(move));
+}
+moveUser(move);
 
+ 
 
 // 3) Створіть массив в якому видаляється кожний другий елемент ["Keep", "Remove", "Keep", "Remove", "Keep", ...] в результаті повинен бути ось такий новий массив ["Keep", "Keep", "Keep", ...], Врахувати що массив може бути пустий, повернути помилку в разі пустого масиву
 let elements = [];
@@ -84,13 +135,13 @@ square(figurs);
 
 // 5) Створити новий массив який використовує массив [2,3,5,4,8,7,9,10] і перемножує парні значення на 4
 let NumbersArr = [2,3,5,4,8,7,9,10];
-let multiArr = [];
-for(let n=0; n<NumbersArr.length; n++){
-  if(NumbersArr[n]%2 == 0){
-    let multiple = NumbersArr[n] * 4;
-    // console.log(multiple);
-    multiArr.push(multiple);
-    // console.log(multiArr);
-  }
+
+function multiple(value, index, array) {
+    let newArray = [];
+    newArray[index] = (value%2 == 0)?array[index] = value * 4:array[index] = value;
+    // if(value%2 == 0){
+    //     array[index] = value * 4;
+    // }
 }
-console.log(multiArr)
+NumbersArr.forEach(multiple);
+console.log(NumbersArr);
